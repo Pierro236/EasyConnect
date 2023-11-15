14c2b34b293c312b1814108ab8f0c5fc9851724b
Monica <a01707439@itesm.mx>
2023-11-15 18:41:55 +0100

    [FRONT] Profile Information buttons

--- components/ProfileInfo.tsx
+++ components/ProfileInfo.tsx
@@ -1,23 +1,36 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Button } from "react-native";
 import Avatar from "./Avatar";
 import StatsCard from "./StatsCard";
+import CustomButton from "./CustomButton";
 
 interface ProfileInfoProps {
-    name: string;
-    description?: string;
-  }
+  name: string;
+  description?: string;
+}
 
 const ProfileInfo: React.FC<ProfileInfoProps> = ({ name, description }) => {
   return (
     <View style={styles.container}>
       <View style={styles.additionalInformation}>
-      <View style={styles.userInformation}>
-        <Text style={styles.title}>Bonjour, je m'apelle {name}</Text>
-        <Text style={styles.description}>{description}</Text>
+        <View style={styles.userInformation}>
+          <Text style={styles.title}>Bonjour, je m'apelle {name}</Text>
+          <Text style={styles.description}>{description}</Text>
         </View>
-        <View style={styles.followerInformation}>
         <StatsCard followers={0} following={0} posts={0} />
+        <View style={styles.buttonContainer}>
+          <View style={styles.buttonChildren}>
+            <CustomButton
+              text="Modifier mon profil"
+              onPress={() => console.log("Follow")}
+              buttonStyle={styles.customButtonStyle}
+            />
+            <CustomButton
+              text="Déconnexion"
+              onPress={() => console.log("Message")}
+              buttonStyle={styles.customButtonStyle}
+            />
+          </View>
         </View>
       </View>
       <View style={styles.imageContainer}>
@@ -25,23 +38,22 @@ const ProfileInfo: React.FC<ProfileInfoProps> = ({ name, description }) => {
       </View>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: "row", 
-    backgroundColor: 'white', 
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: "row",
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
   },
   additionalInformation: {
     flex: 5,
+    marginLeft: "10%",
   },
   userInformation: {
-    alignItems: "center",
-  },
-  followerInformation: {
+    flexDirection: "column",
   },
   imageContainer: {
     flex: 2,
@@ -54,6 +66,19 @@ const styles = StyleSheet.create({
   },
   description: {
     fontSize: 23,
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  buttonChildren: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  customButtonStyle: {
+    marginRight: 20,
   },
 });
 
