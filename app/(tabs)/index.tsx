26d1000a8330b77c2d05cfd9950de1d48cbefd46
Monica <a01707439@itesm.mx>
2024-01-29 23:00:12 +0100

    Map posts in feed to database

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,81 @@
-import { Image, StyleSheet, View, Text, ScrollView } from "react-native";
+import { StyleSheet, View, Text, ScrollView } from "react-native";
 const testimage = require("../../assets/images/test.jpg");
 import ProfileIcon from "../../components/ProfileIcon";
 import Post from "../../components/PostCard";
-import { fakePostData } from "../../data/post";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { createClient } from "@supabase/supabase-js";
+import { useState, useEffect } from "react";
+import { IPost } from "../../types/index";
+import RecentPosts from "../../components/RecentPosts";
+
+const supabase = createClient(
+  "https://dkabcacfgilbdqnwnbzj.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRrYWJjYWNmZ2lsYmRxbnduYnpqIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTgyNDQ4NDQsImV4cCI6MjAxMzgyMDg0NH0.qE16p_x2DQXowW26cUFeD-SFLsVqXhz0_0hsxx4QYCU"
+);
+
+// MISSING: Do not show your own posts in feed.
 
 export default function TabOneScreen() {
-  const posts = fakePostData;
+  const [posts, setPosts] = useState<IPost[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        // Step 1: Fetch all users
+        const { data: userData, error: userError } = await supabase
+          .from("users")
+          .select("id, username, profile_picture, certified, description, name");
+
+        if (userError) {
+          throw userError;
+        }
+
+        if (userData) {
+          // Step 2: Map users into a dictionary
+          const userDictionary = userData.reduce((acc:any, user) => {
+            acc[user.id] = user;
+            return acc;
+          }, {});
+
+          // Step 3: Fetch posts
+          const { data: postsData, error: postError } = await supabase
+            .from("posts")
+            .select("id, user_id, caption, image, title, created_at")
+            .order("created_at", { ascending: false });
+
+          if (postError) {
+            throw postError;
+          }
+
+          if (postsData) {
+            // Process posts and fill in user details
+            const processedData = postsData.map((post) => ({
+              id: post.id,
+              title: post.title,
+              caption: post.caption,
+              image: post.image,
+              createdAt: post.created_at,
+              user_id: post.user_id,
+                user: userDictionary[post.user_id as string] || {}, // Fill in user details
+              time: post.created_at,
+              likes: 0,
+              comments: 0,
+              liked: false,
+              saved: false,
+              ingredients: 'test',
+            }));
+            console.log(processedData);
+            setPosts(processedData);
+          }
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    
+    fetchData();
+  }, []);
+
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.addPostContainer}>
@@ -17,9 +86,7 @@ export default function TabOneScreen() {
         style={styles.postsContainer}
         showsVerticalScrollIndicator={false}
       >
-        {posts.map((post) => (
-          <Post key={post.id} post={post} />
-        ))}
+        <RecentPosts posts={posts}/>
       </ScrollView>
     </SafeAreaView>
   );
